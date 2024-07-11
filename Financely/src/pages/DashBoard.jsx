91da1b950ac29ba92030c2135c3c16377545ba71
Nainee99 <nainee909@gmail.com>
2024-07-11 11:35:57 +0500

    Ant Design And Dashboard

--- Financely/src/pages/DashBoard.jsx
+++ Financely/src/pages/DashBoard.jsx
@@ -1,10 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
+import Cards from "../components/Card";
+import { Modal } from "antd";
+import AddExpenseModal from "../components/Modal/addExpense";
+import AddIncomeModal from "../components/Modal/addIncome";
 
 const DashBoard = () => {
+  const [isExpenseModalVisible, setIsExpenseModalVisible] = useState(false);
+  const [isIncomeModalVisible, setIsIncomeModalVisible] = useState(false);
+
+  const [transactions, setTransactions] = useState([]);
+
+  const showExpenseModal = () => {
+    setIsExpenseModalVisible(true);
+  };
+
+  const showIncomeModal = () => {
+    setIsIncomeModalVisible(true);
+  };
+
+  const handleExpenseCancel = () => {
+    setIsExpenseModalVisible(false);
+  };
+
+  const handleIncomeCancel = () => {
+    setIsIncomeModalVisible(false);
+  };
+
+  const onFinish = (values, type) => {
+    const newTransaction = {
+      type: type,
+      date: moment(values.date).format("YYYY-MM-DD"),
+      amount: parseFloat(values.amount),
+      tag: values.tag,
+      name: values.name,
+    };
+
+    setTransactions([...transactions, newTransaction]);
+    setIsExpenseModalVisible(false);
+    setIsIncomeModalVisible(false);
+    addTransaction(newTransaction);
+    calculateBalance();
+  };
   return (
     <div>
       <Header />
+      <Cards
+        showExpenseModal={showExpenseModal}
+        showIncomeModal={showIncomeModal}
+      />
+      <AddExpenseModal
+        isExpenseModalVisible={isExpenseModalVisible}
+        handleExpenseCancel={handleExpenseCancel}
+        onFinish={onFinish}
+      />
+      <AddIncomeModal
+        isIncomeModalVisible={isIncomeModalVisible}
+        handleIncomeCancel={handleIncomeCancel}
+        onFinish={onFinish}
+      />
     </div>
   );
 };
